7901cb42d407ce3a126942b37ee261db207774c7
Justin Wong <registered99@gmail.com>
2013-08-27 17:00:38 -0400

    Change single touch to multitouch

--- Assets/MovingCharactertest/Scripts/InGamePage.cs
+++ Assets/MovingCharactertest/Scripts/InGamePage.cs
@@ -1,37 +1,34 @@
 using System;
 using UnityEngine;
-namespace move_character_space {
-	public class InGamePage : FContainer, FSingleTouchableInterface
+
+namespace move_character_space
+{
+	public class InGamePage : FContainer, FMultiTouchableInterface
 	{
 		public Vector2 touchPos;
 		bool touchInput = false;
 		BCharacter character;
+
 		public InGamePage ()
 		{
-			EnableSingleTouch(); //IMPORTANT!!
-			character = new BCharacter();
+			EnableMultiTouch (); //IMPORTANT!!
+			character = new BCharacter ();
 			character.scale = 0.25f;
-			AddChild(character);
-			ListenForUpdate(HandleUpdate);
+			AddChild (character);
+			ListenForUpdate (HandleUpdate);
 		}
-	public bool HandleSingleTouchBegan(FTouch touch){
-		touchPos = touch.position;
-		touchInput = true;
-	    return true;	
-	}
-	public void HandleSingleTouchMoved(FTouch touch){
-		touchPos = touch.position;
-	}
-	public void HandleSingleTouchEnded(FTouch touch){
-		
-	}
-	public void HandleSingleTouchCanceled(FTouch touch){
-			
+
+		private void HandleUpdate ()
+		{
+			character.x = touchPos.x;	
+			character.y = touchPos.y;
 		}
-	private void HandleUpdate(){
-		character.x = touchPos.x;	
-		character.y = touchPos.y;
+		public void HandleMultiTouch(FTouch[] touches)
+		{
+			if (touches.Length > 0){
+				touchPos = touches[0].position;
+			}
+		}
 	}
-	}
-	}
+}
 
